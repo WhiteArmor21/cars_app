3c564fbdd9bfb51cfecfc57bf0617fac4c4f5bdb
WhiteArmor21 <97619278+WhiteArmor21@users.noreply.github.com>
2022-10-19 12:29:57 +0300

    Update settings.py

--- app/django_project/settings.py
+++ app/django_project/settings.py
@@ -21,6 +21,9 @@ DEBUG = int(environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(' ')
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -35,6 +38,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
