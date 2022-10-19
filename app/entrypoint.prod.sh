b91068c93777ccd99a483e642c1c65979bc178d3
WhiteArmor21 <97619278+WhiteArmor21@users.noreply.github.com>
2022-10-19 10:34:50 +0300

    Update entrypoint.prod.sh

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -21,8 +21,7 @@ then
     python manage.py createsuperuser \
         --noinput \
         --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL \
-        --password $DJANGO_SUPERUSER_PASSWORD
+        --email $DJANGO_SUPERUSER_EMAIL
 fi
 
 exec "$@"
