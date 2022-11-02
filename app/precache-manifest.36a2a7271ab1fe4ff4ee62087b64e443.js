305dd0110c8230366a99b7687c8fc049692d7a27
Denis <ushakov.denis123@gmail.com>
2022-11-02 21:40:45 +0300

    added frontend

---
+++ app/precache-manifest.36a2a7271ab1fe4ff4ee62087b64e443.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9a6de19e36e260da428",
+    "url": "/static/css/main.d7bc846c.chunk.css"
+  },
+  {
+    "revision": "e9a6de19e36e260da428",
+    "url": "/static/js/main.e9a6de19.chunk.js"
+  },
+  {
+    "revision": "74b19d40e79038da8a87",
+    "url": "/static/css/1.124c86a2.chunk.css"
+  },
+  {
+    "revision": "74b19d40e79038da8a87",
+    "url": "/static/js/1.74b19d40.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e527cec4d7fbe80b5bfd0638f033231",
+    "url": "/static/media/logo.2e527cec.png"
+  },
+  {
+    "revision": "b9fb6d6603fbb54d8f9d478c77008808",
+    "url": "/static/media/rolls.b9fb6d66.png"
+  },
+  {
+    "revision": "1fc6a83d1e366dfe7babb690d33ba6fb",
+    "url": "/static/media/Robotobold.1fc6a83d.ttf"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "91a5c93a35e51736c8b54b4a234a093d",
+    "url": "/static/media/background.91a5c93a.png"
+  },
+  {
+    "revision": "7a8c57d20ae42c032dd31e2d689cf064",
+    "url": "/index.html"
+  }
+];
